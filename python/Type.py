700ff6e2a975451b810c608e49a45c842a966c8b
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-16 19:42:25 +0530

    Create Type.py

---
+++ python/Type.py
@@ -0,0 +1,12 @@
+#the program demonstrate the data type of the python which we do not have to declare specifically
+
+Name = "Shailesh"
+marks = 80.43
+Data = 45+4j
+food = False
+
+print(type(Name))
+print(type(marks))
+print(type(Data))
+print(type(food))
+
