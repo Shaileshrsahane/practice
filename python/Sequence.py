be2b06443e40ba89d3a3691a03e4e5f812a7a023
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-16 20:14:38 +0530

    Create Sequence.py

---
+++ python/Sequence.py
@@ -0,0 +1,16 @@
+#sequence contains 3 data type 1. List  2. Tuple  3. Range
+
+# List
+Value1 = [10,20,30,40,50]
+print(Value1)
+print(type(Value1))
+
+#Tuple
+Value2 = (10,20,30,40,10)
+print(Value2)
+print(type(Value2))
+
+#Set
+Value3 = {10,20,30,40,10}
+print(Value3)
+print(type(Value3))
