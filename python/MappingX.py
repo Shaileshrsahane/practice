ae2b16cc79a35d7f25209d24f31cd2c750d5c2c0
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-16 22:53:24 +0530

    Create MappingX.py

---
+++ python/MappingX.py
@@ -0,0 +1,7 @@
+#In dictionary data type we can change the value by using the key
+
+Information = {"Name": "Shailesh", "Age" : 25, "City" : "Pune", "marks" : 88.00}
+print(Information)
+
+Information["Age"] = 26
+print(Information)
