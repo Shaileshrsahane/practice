189b808d679ecec3f6480aaebbc524b6d30cbdf8
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 12:10:05 +0530

    Create Size.py

---
+++ python/Size.py
@@ -0,0 +1,15 @@
+#in the python the sys is a inbuilt library which contains multiple functions
+#here the getsizeof is the method which gives the actual size get by the variable to store the data
+
+import sys
+
+data = 12222222333333333333344444445
+
+marks = 69.5
+name = "Shailesh"
+
+print(sys.getsizeof(data))
+print(sys.getsizeof(marks))
+print(sys.getsizeof(name))
+
+
