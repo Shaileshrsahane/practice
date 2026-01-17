0fe25dada71c1a146073d792d3c3bd7b1da292cd
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 12:14:48 +0530

    Create IO.py

---
+++ python/IO.py
@@ -0,0 +1,10 @@
+#in the python bydefault input gets taken into the string 
+
+print("Enter your name")
+Name = input()
+
+print("Enter your age")
+Age = input()
+
+print("name is : ",Name)
+print("age is : ",Age)
