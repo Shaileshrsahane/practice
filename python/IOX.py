75cf8b69c538e5a11a2857443805542d1a335296
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 12:20:24 +0530

    Create IOX.py

---
+++ python/IOX.py
@@ -0,0 +1,8 @@
+#in python we can give output parameter into  the input function so the input function can display data
+
+Name = input("Enter you name : ")
+
+Age = input("Enter you Age : ")
+
+print("Name is : ",Name)
+print("Age is : ",Age)
