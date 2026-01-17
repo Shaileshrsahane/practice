cd387c7f88d1696876cc33ad8f7b31d2c01ae9c7
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 13:19:29 +0530

    Create IOXXX.py

---
+++ python/IOXXX.py
@@ -0,0 +1,15 @@
+print("Enter first value")
+No1 = int(input()) #while accepting the data we type cast it so it will directly store as int in No1
+
+print("Enter second value")
+No2 =int(input())   
+
+print(type(No1))
+print(type(No2))
+
+Ans = No1 + No2
+
+print("Ans is : ",Ans)
+
+print(type(No1))
+print(type(No2))
