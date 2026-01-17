c1f3f800b860043296f4c487cbe48d6b0a632c2e
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 12:27:05 +0530

    Create IOXX.py

---
+++ python/IOXX.py
@@ -0,0 +1,16 @@
+print("Enter first value")
+No1 = input() #by default in string
+
+print("Enter second value")
+No2 = input()   #by default in string
+
+print(type(No1))
+print(type(No2))
+
+Ans = int(No1) + int(No2) #for doing operation on it we need to typecast it . it changes from str to int temprorarily
+
+print("Ans is : ",Ans)
+
+print(type(No1))
+print(type(No2))
+
