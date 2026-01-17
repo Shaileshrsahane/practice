5267cdb74d2a986000bd9f7aa9f69085c357125e
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2026-01-17 13:36:40 +0530

    Create Range_1.py

---
+++ python/Range_1.py
@@ -0,0 +1,9 @@
+#it is range datatype from sequence. it takes 3 parmeter the 1st and last parameter is optional
+
+data = range(7)
+print(data)
+print(type(data))
+
+test = range(3,9,2)
+print(test)
+print(type(test))
